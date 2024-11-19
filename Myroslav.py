14ff3148a3a67d48d9209ef1757eddb818644c1f
f1serty <miroslav2103bragnik@gmail.com>
2024-11-19 18:44:10 +0100

    created code

---
+++ Myroslav.py
@@ -0,0 +1,115 @@
+import telebot
+from telebot.types import InlineKeyboardButton,InlineKeyboardMarkup
+import filmsM
+bot=telebot.TeleBot("7706935658:AAHvUzDDHGoh6v54YEwEqsQ-Rn69m7Azd9o")
+
+@bot.message_handler(commands=["start"])
+def Start(msg):
+    KB=InlineKeyboardMarkup()
+    KB.add(InlineKeyboardButton("Help",callback_data="Help"))
+    bot.send_message(msg.chat.id,"Привіт,Я Бот для пошуку філмів",reply_markup=KB)
+
+@bot.message_handler(commands=["help"])
+def Help(msg):
+    KB=InlineKeyboardMarkup()
+    KB.add(InlineKeyboardButton("films",callback_data="films"))
+    bot.send_message(msg.chat.id,"Привіт,Я Бот для пошуку філмів",reply_markup=KB)
+
+
+
+@bot.message_handler(commands=["film"])
+def film(msg):
+    KB=InlineKeyboardMarkup()
+    films=[InlineKeyboardButton(film,callback_data=f"film:{film}")for film in filmsM.Biblioteka]
+    KB.add(*films)
+    bot.send_message(msg.chat.id,"Привіт,Я Бот для пошуку філмів",reply_markup=KB)
+
+
+
+@bot.callback_query_handler(func=lambda call:True)
+
+def Button(call):
+    if call.data=="Help":
+         KB=InlineKeyboardMarkup()
+         KB.add(InlineKeyboardButton("films",callback_data="films"))
+         bot.answer_callback_query(call.id)
+         bot.send_message(call.message.chat.id,"Привіт,Я Бот для пошуку філмів",reply_markup=KB)
+    elif call.data=="films":
+         KB=InlineKeyboardMarkup()
+         films=[InlineKeyboardButton(film,callback_data=f"film:{film}")for film in filmsM.Biblioteka]
+         KB.add(*films)
+         bot.answer_callback_query(call.id)
+         bot.send_message(call.message.chat.id,"Привіт,Я Бот для пошуку філмів",reply_markup=KB)
+    else:
+        name=call.data.replace("film:","")
+        if name in filmsM.Biblioteka:
+            info=filmsM.Biblioteka[name]
+            foto=info.get("Фото","")
+            response = f"{name} ({info['Рік']})\nРежисер: {info['Режисер']}\nЖанр: {info['Жанр']}"
+            if foto:
+                bot.send_photo(call.message.chat.id,foto,caption=response)
+            else:
+                bot.send_message(call.message.chat.id,response)
+
+
+
+@bot.message_handler(commands=['addfilm'])
+def add(msg):
+    bot.reply_to(msg,"Введіть назву фільму")
+    bot.register_next_step_handler(msg,adddirector)
+
+def adddirector(message):
+    user_data={}
+    user_data["name"]=message.text
+    bot.reply_to(message, f"Введіть режисера фільму")
+    bot.register_next_step_handler(message, add_year, user_data)
+def add_year(message,user_data):
+    user_data["director"]=message.text
+    bot.reply_to(message, f"Введіть рік випуску фільму")
+    bot.register_next_step_handler(message, add_genre, user_data)
+def add_genre(message,user_data):
+    user_data["year"]=message.text
+    bot.reply_to(message, f"Введіть жанр фільму")
+    bot.register_next_step_handler(message, add_photo, user_data)
+def add_photo(message,user_data):
+    user_data["genre"]=message.text
+    bot.reply_to(message, f"Введіть посилання на фото фільму")
+    bot.register_next_step_handler(message, add_save, user_data)
+def add_save(message,user_data):
+    filmsM.Biblioteka[user_data['name']]={
+        'Режисер':user_data['director'],
+        'Рік':user_data['year'],
+        'Жанр':user_data['genre'],
+        "Фото":message.text
+    }
+    bot.reply_to(message,"Філм додано до бібліотеки")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.polling()
---
+++ filmsM.py
@@ -0,0 +1,32 @@
+Biblioteka={
+    "Людина Павук":
+    {
+        "Режисер":"Сєм Реймі",
+        "Рік":"2002",
+        "Жанр":"Бойовик,Трилер,Драма",
+        "Фото":"https://cdn-ksvod.kyivstar.ua/content/HLS/VOD/IMAGE2/5ba21fbb60b27f7ebcc82189/IMAGE_2_3_XL.png"
+    },
+    "Кобра Кай":
+    {
+        "Режисер":"Джон Харвитц, Хейден Шлоссберг, Джош Хилд, Дженнифер Челотта",
+        "Рік":"2018",
+        "Жанр":"Комедія,Драма,Бойові Мистецтва",
+        "Фото":"https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/37993232-4204503.jpg"
+    },
+
+    "Молодёжка":
+    {
+        "Режисер":"Вячеслав Муругов",
+        "Рік":"2013",
+        "Жанр":"Спорт",
+        "Фото":"https://avatars.dzeninfra.ru/get-zen_doc/34175/pub_59d692dd8c8be380f8aebc76_59d77c1b256d5c7d9e9ac865/scale_1200"
+    },
+
+"ЛЮДИ X Початок Расомаха":
+    {
+        "Режисер":"Гэвин Худ",
+        "Рік":"2009",
+        "Жанр":"Боевик/Научная фантастика",
+        "Фото":"https://upload.wikimedia.org/wikipedia/ru/f/ff/X-Men-Origins_Wolverine.jpg"
+    },
+}
